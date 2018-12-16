ba48dec7ddee93f67327203a28200eae7b0c5262
roecodes <roe.j.emma@gmail.com>
2018-12-15 19:50:34 -0500

    initial commit :fire:

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+$(function () { // start document ready 
+  
+  // let particles = Particles.init({
+  //   selector: '.triangles',
+  //   color: ['#DA0463', '#404B69', '#DBEDF3'],
+  //   sizeVariations: 0,
+  //   maxParticles: 500,
+  // }); 
+
+  // particlesJS.load('particles-js', './assets/particles.json', function () {
+
+  // });
+
+}); // end of document ready 
+
