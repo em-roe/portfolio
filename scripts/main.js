c270c6f4476cbe0677186a8179e576d3a14f1240
roecodes <roe.j.emma@gmail.com>
2018-12-19 11:23:59 -0500

    getting very close

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,72 @@ $(function () { // start document ready
   
   // $(".name").hover(
   //   function () {
-  //   $('.name').css("mix-blend-mode", "difference");
+  //   $('.one').addClass('animated hinge');
   // }, 
   // function () {
-  //   $('.name').css("mix-blend-mode", "multiply");
+  //   // $('.one').removeClass('animated hinge');
   // });
+  
     
+  // $(".name").on('click', function(){
 
+  //   $('.one').addClass('animated hinge').one('animationend',
+  //   function() {
+  //     $('.one').removeClass('animated hinge');
+  //   })
+
+  // });
+
+  // $(".six").on('click', function () {
+  //   $('.three').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+
+  // $(".one").on('click', function () {
+  //   $('.seven').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+
+  // $(".two").on('click', function () {
+  //   $('.five').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+
+  // $(".five").on('click', function () {
+  //   $('.four').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+  // $(".four").on('click', function () {
+  //   $('.six').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+
+  // $(".three").on('click', function () {
+  //   $('.one').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+  // $(".three").on('click', function () {
+  //   $('.two').addClass('animated hinge').one('animationend',
+  //     function () {
+  //       $(this).removeClass('animated hinge');
+  //     })
+  // });
+
+
+ 
+  
 
   // $(".name").hover(
   //   function () {
